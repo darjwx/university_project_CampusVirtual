780eb60edf23dba3330fea09c129cfbe5776b4a8
darjwx <darjwx@gmail.com>
2018-10-17 12:56:00 +0200

    project: Add an early-stage resources class

---
+++ Resources.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Resources.h"
+
+using namespace std;
+
+Resources::Resources(char status, char id) {
+  this->status = status;
+  this->id = id;
+}
+
+void Resources::setStatus(char status) {
+  this->status = status;
+}
+
+char Resources::getStatus() {
+  return status;
+}
+
+void Resources::setId(char id) {
+  this->id = id;
+}
+
+char Resources::getId() {
+  return id;
+}
---
+++ Resources.h
@@ -0,0 +1,18 @@
+#ifndef _Resources_H_
+#define _Resources_H_
+class Resources {
+private:
+  //c: creado, i: iniciado, f:finalizado
+  char status;
+  //LLLNNNN: L, letra entre Aa y Zz. N, numero del 0 al 9
+  char id;
+public:
+  Resources();
+  Resources(char status, char id);
+  ~Resources();
+  void setStatus(char status);
+  char getStatus();
+  void setId(char id);
+  char getId();
+};
+#endif //_Resources_H_
