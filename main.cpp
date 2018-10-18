ad52ef1df33208346327057f02182af1280d32af
darjwx <darjwx@gmail.com>
2018-10-18 17:37:23 +0200

    main: call the inicio method

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "main.h"
+#include "CampusVirtual.h"
 
 using namespace std;
 
-int main() {}
+int main() {
+  CampusVirtual cv = CampusVirtual();
+  cv.inicio();
+
+  return 0;
+}
