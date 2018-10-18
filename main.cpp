9706b047060da4b99c4bd900233117886c016060
darjwx <darjwx@gmail.com>
2018-10-18 17:53:22 +0200

    main: add an early-stage function to get all the settings

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include "main.h"
 #include "CampusVirtual.h"
 
 using namespace std;
 
 int main() {
-  CampusVirtual cv = CampusVirtual();
-  cv.inicio();
+  cargarArchivo();
+
+  //CampusVirtual cv = CampusVirtual();
+  //cv.inicio();
 
   return 0;
+}
+
+void cargarArchivo() {
+  ifstream memoria("ajustes.txt");
+
+  //Test
+  char text[100];
+  memoria.getline(text, 100);
+  cout<<text<<endl;
+
 }
--- main.h
+++ main.h
@@ -1,3 +1,4 @@
 #ifndef _main_H_
 #define _main_H_
+void cargarArchivo();
 #endif //_main_H_
