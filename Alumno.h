7028a44f5c1fa51f3098ad182c9092243c01fc13
darjwx <darjwx@gmail.com>
2018-10-24 14:26:34 +0200

    Alumno: add a few attributes and functions to de developed in the future

--- Alumno.h
+++ Alumno.h
@@ -1,6 +1,17 @@
 #ifndef _Alumno_H_
 #define _Alumno_H_
 class Alumno {
+private:
+  int nia;
+  char nombre;
+public:
+  Alumno();
+  Alumno(int nia, char nombre);
+  void setNombre(char nombre);
+  void setNia(int nia);
+  int getNia();
+  char getNombre();
+  void getLista();
 
 };
 #endif //_Alumno_H_
